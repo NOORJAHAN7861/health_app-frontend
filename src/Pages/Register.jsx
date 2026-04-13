c724d5333e7a779e643e3f25ac54d309b694e7bd
unknown <noorjahanpp786@gmail.com>
2026-04-13 16:34:15 +0530

    .

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -24,7 +24,7 @@ const Register = () => {
 
     try {
       const { data } = await api.post(
-        "/api/v1/user/register",
+        "/user/patient/register",
         {
           firstName,
           lastName,
